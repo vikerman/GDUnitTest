64086e3c6ef9c166aa0fc1f259f9c09b623505b2
Vikram Subramanian <vikram.subramanian@gmail.com>
2024-03-09 21:51:30 +0100

    Run the tests using the res:// for script invocation

--- addons/gdUnit4/runtest.sh
+++ addons/gdUnit4/runtest.sh
@@ -10,6 +10,6 @@ $GODOT_BIN --path . -s -d ./addons/gdUnit4/bin/GdUnitCmdTool.gd $*
 exit_code=$?
 echo "Run tests ends with $exit_code"
 
-$GODOT_BIN --headless --path . --quiet -s -d ./addons/gdUnit4/bin/GdUnitCopyLog.gd $* > /dev/null
+$GODOT_BIN --headless --path . --quiet -s -d res://addons/gdUnit4/bin/GdUnitCopyLog.gd $* > /dev/null
 exit_code2=$?
 exit $exit_code
